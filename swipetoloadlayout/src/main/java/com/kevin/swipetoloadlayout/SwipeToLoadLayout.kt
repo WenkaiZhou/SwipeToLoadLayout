50eaa53e4a0b1dc091b2b4eebdf23686bfd92d66
zhouwenkai <zhouwenkai@baidu.com>
2019-10-01 12:33:43 +0800

    Add LICENSE

--- swipetoloadlayout/src/main/java/com/kevin/swipetoloadlayout/SwipeToLoadLayout.kt
+++ swipetoloadlayout/src/main/java/com/kevin/swipetoloadlayout/SwipeToLoadLayout.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019 Kevin zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kevin.swipetoloadlayout
 
 import android.content.Context
@@ -21,7 +36,7 @@ import kotlin.math.abs
  * SwipeToLoadLayout
  *
  * @author zwenkai@foxmail.com, Created on 2019-09-27 10:34:58
- * Major Function：**SwipeToLoadLayout**
+ * Major Function：<b>SwipeToLoadLayout</b>
  *
  *
  * Note: If you modify this class please fill in the following content as a record.
@@ -1024,7 +1039,7 @@ open class SwipeToLoadLayout @JvmOverloads constructor(
             }
             val targetRight = targetLeft + targetView.measuredWidth
             val targetBottom = targetTop + targetView.measuredHeight
-            
+
             targetView.layout(targetLeft, targetTop, targetRight, targetBottom)
         }
 
