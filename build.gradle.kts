d1a38eaf17d3b28a9e224638a4a8cb4adbf582a6
Kevin.zhou <zwenkai@foxmail.com>
2024-03-05 15:48:30 +0800

    Update gradle version to 7.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
-        classpath("com.vanniktech:gradle-maven-publish-plugin:0.21.0")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.27.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
